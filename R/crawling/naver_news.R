ec294628f7073b9992aa93f13cc4db794211e9c6
Lee <littlecsi27@gmail.com>
2019-11-08 01:39:09 +0900

    Successfully finished crawling from naver news
    There is one error which is that the date is unexpectedly repeating the dates more than 30 times and the values written in the csv file is wrong.

--- R/crawling/naver_news.R
+++ R/crawling/naver_news.R
@@ -9,7 +9,6 @@ get_html <- function(url) {
     html <- read_html(url)
     return(html)
 }
-
 check_cat <- function(vec) {
     len <- length(vec)
     if(len < 30) {
@@ -19,12 +18,32 @@ check_cat <- function(vec) {
         return(vec)
     }
 }
+check_url <- function(url) {
+    out <- tryCatch(
+        {
+            read_html(url)
+            return(T)
+            
+        }, error=function(cond) {
+            message("URL doesn't exist")
+            message(cond)
+            return(FALSE)
+            
+        }, warning=function(cond) {
+            message("URL caused a warning")
+            message(cond)
+            return(FALSE)
+            
+        }, finally={}
+    )
+    return(out)
+}
 
 # Section - Politics, Economics, Society, Culture, World, Science, Photo, TV
 ################################################################################
 sec_id <- as.character(c(100:105))
 
-year <- c(2010:2019)
+year <- c(2018)
 month <- c('01','02','03','04','05','06','07','08','09','10','11','12')
 day <- c(month, as.character(c(13:31)))
 
@@ -37,14 +56,24 @@ df <- data.frame()
 for(y in year) {
     for(m in month) {
         for(d in day) {
-            if(m %in% c('04','06','09','11') & d == 31)
+            # Disgard months with no 31st (except February)
+            if(m %in% c('04','06','09','11') & d == 31) { next }
+            # Disgard February (special cases)
+            if(m %in% c('02') & d >= 30) { next }
+            
+            # Extract all category in this date
             for(id in sec_id) {
-                # print(id)
+                # creates date variable
                 date <- paste(y, m, d, sep='')
-                print(date)
+                print(date) # prints out the date each time to check
 
+                # crates complete url to crawl
                 url <- paste(url_1, url_2, id, url_3, date, sep='')
+                
+                # checks if url is loadable. If not, it jumps to the next one
+                if(check_url(url) == FALSE) { next }
 
+                # loads the HTML code from the URL
                 html <- get_html(url)
                 item <- html %>% html_nodes('.ranking_item')
                 
@@ -56,9 +85,7 @@ for(y in year) {
                 source <- item %>% html_nodes('.ranking_office') %>% html_text()
                 view <- item %>% html_nodes('.ranking_view') %>% html_text()
                 
-                if(length(view) == 0) {
-                    view <- rep(NA, 30)
-                }
+                # if(length(view) == 0) { view <- rep(NA, 30) }
                 
                 title <- check_cat(title)
                 source <- check_cat(source)
