92cf9f1fb7bcf38dcaa4e3df932ba4c5de5f59bd
Lee <littlecsi27@gmail.com>
2019-12-05 18:19:19 +0900

    의사 결정 트리 Finished

--- _05_tree/tree_type.R
+++ _05_tree/tree_type.R
@@ -1,8 +1,17 @@
 ####################################################################################################
 # Library
+library(tree)
+library(rpart)
+library(party)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+library(caret)
+
 library(stringr)
 library(reshape2)
 library(ggplot2)
+library(progress)
 
 source("base/db.R")
 
@@ -10,8 +19,8 @@ source("base/db.R")
 # Variable
 
 # Logic :
-## 종속 변수 : 성별
-## 독립 변수 : 카테고리, 연령
+## 종속 변수 : 카테고리
+## 독립 변수 : 성별, 연령
 
 # 과정 :
 ## 1. 성별과 연령대를 전체 댓글 수에 비례하도록 나눈다.
@@ -28,4 +37,51 @@ Sdf <- getSectionData(sections[5], type)
 Wdf <- getSectionData(sections[6], type)
 
 df <- rbind(Edf, Idf, Ldf, Pdf, Sdf, Wdf)
-colnames(df)
+df$cat <- substr(df$NEWSID, 1, 1)
+
+# Get rid of some columns in data frame
+tree_df <- df[,-c(1, 2, 3, 4, 5, 6, 7)]
+# tree_df <- tree_df %>% subset(NEWSRANK %in% c(1,2,3,4,5))
+
+# Get real values of comments
+pb <- progress_bar$new(
+    format="[:bar] :current/:total (:percent)", total=nrow(tree_df)
+)
+
+pb$tick(0)
+for(i in c(1:nrow(tree_df))) {
+    pb$tick(1)
+    # 5 ~ 12
+    for(j in c(5:12)) {
+        tree_df[i,j] <- round(tree_df[i,1] * tree_df[i,j] / 100, 2)
+    }
+}
+
+# Extracting Independent Variables
+treeData <- tree_df[,-c(1,2,3,4)]
+
+# Create Training and Testing Data
+idx <- createDataPartition(y=treeData$cat, p=0.8, list=F)
+train <- treeData[idx,]
+test <- treeData[-idx,]
+
+##################################################
+### tree package
+treeMod <- tree(cat ~ ., data=train)
+plot(treeMod)
+text(treeMod)
+
+cv.trees <- cv.tree(treeMod, FUN=prune.misclass)
+plot(cv.trees)
+
+##################################################
+### party package
+ctreeMod <- ctree(cat ~ ., data=train)
+plot(treeMod)
+
+##################################################
+### rpart package
+rpartMod <- rpart(cat ~ ., data=train, method="class")
+rpartMod
+
+fancyRpartPlot(rpartMod, caption = NULL)
