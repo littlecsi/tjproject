cf680d0160018881797e6f4b74fcfe139868829a
Lee <littlecsi27@gmail.com>
2019-12-05 16:06:55 +0900

    more test data

--- _05_tree/tree_type.R
+++ _05_tree/tree_type.R
@@ -27,11 +27,5 @@ Pdf <- getSectionData(sections[4], type)
 Sdf <- getSectionData(sections[5], type)
 Wdf <- getSectionData(sections[6], type)
 
-df <- data.frame()
 df <- rbind(Edf, Idf, Ldf, Pdf, Sdf, Wdf)
-
-nrow(df)
 colnames(df)
-df <- df[, -c(3, 4, 5, 7)]
-df$category <- 
-table(str_sub(df$NEWSID, 1, 1))
