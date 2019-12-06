f43c63cdca5fa2c4d4f82ed580dbd8efe9c94a70
Rap0d <40348849+Rap0d@users.noreply.github.com>
2019-12-06 15:43:56 +0900

    update file

--- _09_KNN/KNN.R
+++ _09_KNN/KNN.R
@@ -5,28 +5,26 @@ library(reshape2)
 library(ggplot2)
 library(scales)
 library(class)
+library(progress)
 
 source("base/db.R")
 
 ### Function
-
-# data frame에 댓글 수가 우세한 성별 수치 추가
-## 0 : 남자와 여자의 비율이 같음 
-## 1 : 남자가 댓글이 많음
-## 2 : 여자가 댓글이 많음
-addGender <- function(data) {
-    data$gender <- ifelse(data[,1] > data[,2], 1, 
-                          ifelse(data[,1] == data[,2], 0, 2))
-    return(data)
-}
-
-# data frame에 댓글 수가 우세한 연령대 컬럼 추가
-addAge <- function(data) {
-    data$age <- 99
-    for(i in c(1:nrow(data))) {
-        data[i,11] <- which.max(data[i,3:8]) * 10
+changeNum <- function(df) {
+    # Get real values of comments
+    pb <- progress_bar$new(
+        format="[:bar] :current/:total (:percent)", total=nrow(df)
+    )
+    
+    pb$tick(0)
+    for(i in c(1:nrow(df))) {
+        pb$tick(1)
+        # 5 ~ 12
+        for(j in c(2:9)) {
+            df[i,j] <- round(df[i,1] * df[i,j] / 100, 2)
+        }
     }
-    return(data)
+    return(df)
 }
 
 ### Main
@@ -36,73 +34,149 @@ type <- 'C'
 S_training <- getSectionData(sections[5], 'C')
 C_training <- getSectionData(sections[3], 'C')
 
-S_testing <- getTestData(sections[5], 'C')
-C_testing <- getDataWithQuery('select * from 2019_comment_data_life_cult')
-
 # data preprocessing
 S_training$cate <- substr(S_training$NEWSID, 1, 1)
 S_training <- S_training[,c(-1,-7)]
 
-S_testing$cate <- substr(S_testing$NEWSID, 1, 1)
-S_testing <- S_testing[,c(-1,-7)]
-
 C_training$cate <- substr(C_training$NEWSID, 1, 1)
 C_training <- C_training[,c(-1,-7)]
 
-C_testing$cate <- 'L'
-
-colnames(C_testing) <- colnames(C_training)
-
 tot_training <- rbind(S_training, C_training)
-tot_testing <- rbind(S_testing, C_testing)
-
-training <- tot_training[, c(10:18)]
-testing <- tot_testing[, c(10:18)]
+training <- tot_training[, c(6, 10:18)]
 
 # 네이버의 댓글 정책으로 인한 통계가 없는 데이터 삭제
 training <- subset(training, training$X30 != 0)
-testing <- subset(testing, testing$X30 != 0)
+training <- changeNum(training)
 
-# 성별 컬럼 추가
-training <- addGender(training)
-testing <- addGender(testing)
+training <- training[,c(-1)]
 
-training <- addAge(training)
-testing <- addAge(testing)
+# # training : test : valid -> 3:1:1로 갖춤
+idx <- sample(x = c("train", "valid", "test"), size = nrow(training), replace = TRUE, prob = c(3, 1, 1))
 
-training <- training[,c(9:11)]
-test <- testing[,c(9:11)]
+# idx에 따라 데이터 나누기
+train <- training[idx == "train", ]
+valid <- training[idx == "valid", ]
+test <- training[idx == "test", ]
 
-# training : test : valid -> 3:1:1로 갖춤
-idx <- sample(1:nrow(training), nrow(test))
-tIdx <- sample(1:nrow(training), nrow(test) * 3)
-train <- training[tIdx, ]
-tmp <- training[-tIdx, ]
-valid <- na.omit(tmp[idx, ])
 
 # 입력x과 출력y 데이터로 분리
 
-train_x <- train[, -1]
-valid_x <- valid[, -1]
-test_x <- test[, -1]
+train_x <- train[, -9]
+valid_x <- valid[, -9]
+test_x <- test[, -9]
 
-train_y <- train[, 1]
-valid_y <- valid[, 1]
-test_y <- test[, 1]
-
-# train 산점도 : train data와 test, valid dat와 비슷한 경향을 보임을 알 수 있다. 
-plot(formula = gender ~ age, data = train, col = alpha(c('purple', 'blue'), 0.7)[train$cate], main = 'train = Classification Category')
-
-points(formula = gender ~ age, data = valid, pch = 17, cex = 1.2, col = 'red')
-points(formula = gender ~ age, data = test, pch = 15, cex = 1.2, col = 'orange')
+train_y <- train[, 9]
+valid_y <- valid[, 9]
+test_y <- test[, 9]
 
 # knn 알고리즘 적용하기(k = 1)
 # k = 1 일 때
 knn_1 <- knn(train = train_x, test = valid_x, cl = train_y, k = 1, use.all = F)
-# Error in knn(train = train_x, test = valid_x, cl = train_y, k = 1, use.all = F) : 
+
+# 분류 정확도 계산하기
+table(knn_1, valid_y)
+# valid_y
+# knn_1     L       S
+# L         1125    233
+# S         216     1946
+
+accuracy_1 <- sum(knn_1 == valid_y) / length(valid_y)
+accuracy_1
+# [1] 0.8724432
+
+# k = 21 일 때
+knn_21 <- knn(train = train_x, test = valid_x, cl = train_y, k = 21)
+
+# 분류 정확도 계산하기
+table(knn_21, valid_y)
+# valid_y
+# knn_21    L       S
+# L         1090    38
+# S         251     2141
+
+accuracy_21 <- sum(knn_21 == valid_y) / length(valid_y)
+accuracy_21
+# [1] 0.9178977
+
+# 최적의 k 값 구해보기
+# k가 1부터 train 행 수까지 변화할 때 분류 정확도 구하기
+## 반복문 for 를 이용하여 k가 1부터 train 행 수까지 변화할 때, 
+## 분류 정확도가 몇 % 되는지 그래프를 그려보고 최적의 k를 확인
+
+# 분류 정확도 사전 할당
+accuracy_k <- NULL
+
+# kk가 1부터 train 행 수까지 증가할 때 (반복문)
+pb <- progress_bar$new(
+    format="[:bar] :current/:total (:percent)", total=250
+)
+
+for(idx in c(1:250)){
+    # k가 kk일 때 knn 적용하기
+    knn_k <- knn(train = train_x, test = valid_x, cl = train_y, k = idx)
+    # 분류 정확도 계산하기
+    accuracy_k <- c(accuracy_k, sum(knn_k == valid_y) / length(valid_y))
+    pb$tick(0)
+    pb$tick(1)
+}
+
+# [=========>----------------------------------------------------] 499/4188 ( 12%)
+# Error in knn(train = train_x, test = valid_x, cl = train_y, k = idx) : 
 #     too many ties in knn
-## KNN을 사용하기에 너무 많은 관계가 있는 알고리즘 에러로 인해
-## 뉴스 댓글의 주 연령층, 성별 데이터로 카테고리 예측을 하기에
-## 적절하지 않은 알고리즘이라고 결론지을 수 있다. 
+## 500 이상의 k 값이 에러가 뜨므로, 250까지로 제한해 본다.
+
+# k에 따른 분류 정확도 데이터 생성
+valid_k <- data.frame(k = c(1:250), accuracy = accuracy_k)
+colnames(valid_k)
+
+# k에 따른 분류 정확도 그래프 그리기
+plot(formula = accuracy ~ k, data = valid_k, type = "o", pch = 20, main = "validation - optimal k")
+
+# 분류 정확도가 가장 높으면서 가장 작은 k의 값 구하기
+sort(valid_k$accuracy, decreasing = T)
+maxdata <- max(valid_k$accuracy) # 0.9119555
+maxdata
+# [1] 0.9196023
+
+min_position <- min(which(valid_k$accuracy == maxdata))
+min_position
+# [1] 22
+
+# min_position의 값과 그래프에서 확인해 보면 적정 k값을 확인할 수 있다.
+# 그럼 이제 k가 min_position의 값을 가질 때 모델이 얼마나 분류가 잘 되는지 test 데이터를 이용해서 표현해보자.
+
+# 최적의 k 값에 test 데이터 적용하기
+knn_optimization <- knn(train = train_x, test = test_x, cl = train_y, k = min_position)
+
+# # Confusion Matrix 틀 만들기
+result <- matrix(NA, nrow = 2, ncol = 2)
+rownames(result) <- paste0("real_", c('S', 'L'))
+colnames(result) <- paste0("clsf_", c('S', 'L'))
+result
+
+# # Confusion Matrix 값 입력하기
+result[1, 1] <- sum(ifelse(test_y == "S" & knn_optimization == "S", 1, 0))
+result[2, 1] <- sum(ifelse(test_y == "L" & knn_optimization == "L", 1, 0))
+result[1, 2] <- sum(ifelse(test_y == "S" & knn_optimization == "L", 1, 0))
+result[2, 2] <- sum(ifelse(test_y == "L" & knn_optimization == "S", 1, 0))
+
+result
+#       clsf_S clsf_L
+# real_S   2140     44
+# real_L   1105    250
+
+# 최종 정확도 계산하기
+table(prediction=knn_optimization, answer=test_y)
+# answer
+# prediction    L    S
+# L         1105    44
+# S         250     2140
+
+# 해석
+
+# 정확도
+accuracy <- sum(knn_optimization == test_y) / sum(result)
+accuracy
+# [1] 0.9169257
 
 dbDisconnectAll()
